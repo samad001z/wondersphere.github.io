527900c81835056f200ec41b8d84d0e970ef7396
samad001z <154825456+samad001z@users.noreply.github.com>
2025-11-29 12:56:39 +0530

    Create DestinationTimeline component

---
+++ components/DestinationTimeline.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DestinationTimeline = () => {
+    return (
+        <div>
+            <h2>Destination Timeline</h2>
+            {/* Add timeline items here */}
+            <ul>
+                <li>2025-11-29: Started planning my trip</li>
+                <li>2025-12-15: Booked the flights</li>
+                <li>2026-01-05: Arrived at the destination</li>
+            </ul>
+        </div>
+    );
+};
+
+export default DestinationTimeline;
